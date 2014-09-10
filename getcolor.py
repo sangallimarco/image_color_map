899f7aa4d8626b096dc57b21f1bb783fc12682d3
Marco Sangalli <sangalli.marco@gmail.com>
2014-09-10 23:16:41 +0100

    using rgb space

--- getcolor.py
+++ getcolor.py
@@ -11,18 +11,17 @@ Map image dominant color to palette and return
 class imageMean:
 	def __init__(self):
 		self.img = None
-		self.thumbSize = 200  
+		self.thumbSize = 400  
 		self.map = {
-			'redId': self._toHLS((255,0,0)),
-			'greenId': self._toHLS((0, 255, 0)),
-			'blueId': self._toHLS((0, 0, 255)),
-			'yellowId': self._toHLS((255, 255, 0)),
+			'redId': (255,0,0),
+			'greenId': (0, 255, 0),
+			'blueId': (0, 0, 255),
+			'yellowId': (255, 255, 0),
+			'black': (0, 0, 0),
 
 		}
+		self.background = (255,255,255)
 
-	def _toHLS(self, color):
-		r, g, b = [ x/255.0 for x in color ]
-		return colorsys.rgb_to_hls(r, g ,b)
 
 	def loadImage(self, src):
 		fd = urllib.urlopen(src)
@@ -30,31 +29,37 @@ class imageMean:
 
 		img = Image.open(image)
 		img = img.resize((self.thumbSize, self.thumbSize))
-		img = img.quantize(6)
+		img = img.quantize(3)
 		img = img.convert('RGB')
 
 		self.img = img
 
+	def distance(self, a, b):
+		return (a[0]-b[0])**2 + (a[1]-b[1])**2 + (a[2]-b[2])**2
+		
 	def mean(self):
 		colors = self.img.getcolors()
+		colors.sort()
+		colors.reverse()
 		print colors
 
 		#skip background
 		for color in colors:
-			colorhls = self._toHLS(color[1])
-			if  colorhls[2] < 0.9 and colorhls[2] > 0.1:
+			primary = color[1]
+			distance = self.distance(primary, self.background)
+			if distance > 100:
 				break
 
 		#calculate min distance
-		minDiff = 100
+		mindistance = (255**2) * 3
 		result = 'Error'
 
-		for k,v in self.map.iteritems():
-			diff = abs( v[0] - colorhls[0] )
-			if diff < minDiff:
-				minDiff =diff
-				result = k
-			
+		for id, mapcolor in self.map.iteritems():
+			distance = self.distance(primary, mapcolor)
+			if distance < mindistance:
+				mindistance = distance
+				result = id
+		print mindistance
 		return result
 
 
